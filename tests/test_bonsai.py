6313e3aac098b3c60c6de3edc5d8b6dd407beb0b
“tliu” <terry.liu.y@gmail.com>
2024-06-29 00:59:49 -0700

    Add tests

--- tests/test_bonsai.py
+++ tests/test_bonsai.py
@@ -35,6 +35,19 @@ def test_bonsai_add_leaf():
         Node(id="F", pos=Point(150.0, 550), _is_leaf=True),
     ]
 
+def test_bonsai_prune_leaf():
+    tree = defaultdict(list)
+    tree["A"] = [Node(id="B", pos=Point(0, 0)), Node(id="C", pos=Point(1, 0))]
+    tree["B"] = [Node(id="D", pos=Point(0, 1)), Node(id="E", pos=Point(1, 1))]
+    bonsai = Bonsai(tree)
+    bonsai.prune("E", "B")
+    assert bonsai.list_nodes() == [
+        Node(id="A", pos=Point(0, 0), _is_leaf=False),
+        Node(id="B", pos=Point(-150.0, 275), _is_leaf=False),
+        Node(id="C", pos=Point(150.0, 275), _is_leaf=True),
+        Node(id="D", pos=Point(-150.0, 550), _is_leaf=True),
+    ]
+
 def test_bonsai_prune_subtree():
     tree = defaultdict(list)
     tree["A"] = [Node(id="B", pos=Point(0, 0)), Node(id="C", pos=Point(1, 0))]
@@ -58,4 +71,29 @@ def test_bonsai_no_nodes():
     tree = defaultdict(list)
     with pytest.raises(InvalidTree):
         bonsai = Bonsai(tree)
-    
+   
+def test_bonsai_add_leaf_and_prune_to_root():
+    tree = defaultdict(list)
+    tree["A"] = [Node(id="B", pos=Point(0, 0)), Node(id="C", pos=Point(1, 0))]
+    tree["B"] = [Node(id="D", pos=Point(0, 1)), Node(id="E", pos=Point(1, 1))]
+    bonsai = Bonsai(tree)
+    bonsai.add_leaf("F", "B")
+    bonsai.prune("B", "A")
+    bonsai.prune("C", "A")
+    assert bonsai.list_nodes() == [
+        Node(id="A", pos=Point(0, 0), _is_leaf=False),
+    ]
+
+def test_bonsai_prune_to_root_and_add_leaf():
+    tree = defaultdict(list)
+    tree["A"] = [Node(id="B", pos=Point(0, 0)), Node(id="C", pos=Point(1, 0))]
+    tree["B"] = [Node(id="D", pos=Point(0, 1)), Node(id="E", pos=Point(1, 1))]
+    bonsai = Bonsai(tree)
+    bonsai.prune("B", "A")
+    bonsai.prune("C", "A")
+    bonsai.add_leaf("B", "A")
+    assert bonsai.list_nodes() == [
+        Node(id="A", pos=Point(0, 0), _is_leaf=False),
+        Node(id="B", pos=Point(0.0, 275), _is_leaf=True),
+    ]
+
